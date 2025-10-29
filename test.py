1523eba604d302444d6ddf323be2c92d4fdd1214
codegeekyyy <2k23.cs2312094@gmail.com>
2025-10-29 16:18:43 +0530

    Add import for langchain_groq and print its version

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import langchain
 import transformers
 import streamlit as st
 import langchain_community
+from langchain_groq import ChatGroq
+import langchain_groq
 
 
 
@@ -9,3 +11,4 @@ print(langchain.__version__)
 print(transformers.__version__)
 print(st.__version__)
 print(langchain_community.__version__)
+print(langchain_groq.__version__)
